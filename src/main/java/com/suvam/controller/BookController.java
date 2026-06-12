aa505315c356f72d63f9984e5edb4d3e102b9fed
suvamgurung <gurungsuvam265@gmail.com>
2026-06-12 08:08:18 +0545

    Removed imports

--- src/main/java/com/suvam/controller/BookController.java
+++ src/main/java/com/suvam/controller/BookController.java
@@ -2,7 +2,6 @@ package com.suvam.controller;
 
 import com.suvam.book.Book;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
